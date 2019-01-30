20a79f397b4a7d4612acd7c151f5e4c89a85ee8f
Kevin White <InnocentSmith0934@users.noreply.github.com>
2019-01-30 01:50:21 -0600

    default response of 20, or respond with number of lines requested, or give help

--- fbaas.go
+++ fbaas.go
@@ -2,6 +2,7 @@ package main
 
 import (
         "strconv"
+        "regexp"
         "net/http"
         "log"
         "encoding/json"
@@ -26,8 +27,24 @@ func fizzbuzz(n int) []string {
    return output
 }
 
+var rNum = regexp.MustCompile(`^/\d+$`)
+var rSlash = regexp.MustCompile(`^/$`)
+
 func defaultHandler(w http.ResponseWriter, r *http.Request) {
-  o := fizzbuzz(20)
+  var o []string
+  if rNum.MatchString(r.URL.Path) {
+    p := r.URL.Path
+    s := p[1:]
+    n, er := strconv.Atoi(s)
+    if er != nil {
+      http.Error(w, er.Error(), http.StatusInternalServerError)
+    }
+    o = fizzbuzz(n)
+  } else if rSlash.MatchString(r.URL.Path) {
+    o = fizzbuzz(20)
+  } else {
+    o = append(o, "Help: valid paths are / or /(number). /(number) outputs (number) lines of fizzbuzz. / outputs 20 lines. any other request gets this message")
+  }
 
   js, err := json.Marshal(o)
   if err != nil {
